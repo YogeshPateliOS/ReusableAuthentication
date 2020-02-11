135a05b075aeaf45db9e77f7656e45f964033c61
Yogesh Patel <yogeshp614@gmail.com>
2020-02-11 18:54:25 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
@@ -10,7 +10,6 @@ let package = Package(
     products: [
         .library(name: "ReusableAuthentication",  targets: ["ReusableAuthentication"])
     ],
-    dependencies: [],
     targets: [
         .target(name: "ReusableAuthentication", path: "Resources")
     ]
