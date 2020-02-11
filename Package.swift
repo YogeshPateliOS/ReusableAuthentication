738f39ec87f08d5788dc17966af1327f0a036ea5
Yogesh Patel <yogeshp614@gmail.com>
2020-02-11 18:35:33 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
-// swift-tools-version:5.1
-
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let package = Package(name: "ReusableAuthentication",
-                      platforms: [.macOS(.v10_12),
-                                  .iOS(.v10),
-                                  .tvOS(.v10),
-                                  .watchOS(.v3)],
-                      products: [.library(name: "ReusableAuthentication",
-                                          targets: ["ReusableAuthentication"])],
-                      targets: [.target(name: "ReusableAuthentication",
-                                        path: "Source")],
-                      swiftLanguageVersions: [.v5])
+let package = Package(
+    name: "ReusableAuthentication",
+    products: [
+        .library(name: "ReusableAuthentication",  targets: ["ReusableAuthentication"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "ReusableAuthentication", path: "Resources")
+    ]
+)
