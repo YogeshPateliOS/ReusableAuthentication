9b33dc0819199fbb247417dc1c346572142f2de8
Yogesh Patel <yogeshp614@gmail.com>
2020-02-13 11:40:35 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,9 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "ReusableAuthentication",
-    platforms: [
-        .iOS(.v12),
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
