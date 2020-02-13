38131bcd764a49786786d81486c7bb2db816ee30
Yogesh Patel <yogeshp614@gmail.com>
2020-02-13 11:14:29 +0530

    commit

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -24,7 +24,6 @@ let package = Package(
             path: "Resources"),
          .testTarget(
             name: "ReusableAuthenticationTests",
-            dependencies: [],
-            path: "Tests"),
+            dependencies: ["ReusableAuthentication"]),
     ]
 )
